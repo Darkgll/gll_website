0ee9684003f62b68ab261357eb733b9a5f19e731
Anton Malyshev <sanamyi7@gmail.com>
2023-01-28 21:38:58 +0700

    "About me" page

--- gll_website/urls.py
+++ gll_website/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='about'),
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts, name='posts'),
-    path('post/<str:post_id>', views.post, name='post'),  # TODO make as str:post_name maybe
+    path('post/<str:post_id>', views.post, name='post'),
     path('create-post/', views.create_post, name='create_post'),
     path('update-post/<str:post_id>', views.update_post, name='update_post'),
     path('delete-post/<str:post_id>', views.delete_post, name='delete_post')
